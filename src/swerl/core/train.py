f5d6c883f9f92907326f6322225042bfe2abd9be
Jinjian Liu <leifmax@berkeley.edu>
2025-03-18 03:16:28 -0700

    Small changes. Added GRPO trainer from TRL. Need to integrate Reward functions.

--- src/swerl/core/train.py
+++ src/swerl/core/train.py
@@ -8,8 +8,21 @@ from litellm.exceptions import (
     InvalidRequestError,
     AuthenticationError,
 )
+from trl import GRPOTrainer, ModelConfig, ScriptArguments, TrlParser, get_peft_config
 
 # Use dataset from SWE-Gym as training data
-dataset = load_dataset("SWE-Gym/SWE-Gym", split="train")
+training_data = load_dataset("SWE-Gym/SWE-Gym", split="train")
+
+
+#TODO: Prompt the model to generate patch
 
 #TODO: Add training script
+trainer = GRPOTrainer(
+    model="QwQ-32B",
+    reward_funcs=reward_funcs,
+    args=training_args,
+    train_dataset=training_data,
+    peft_config=get_peft_config(model_args),
+    callbacks=get_callbacks(training_args, model_args),
+    processing_class=tokenizer,
+)
